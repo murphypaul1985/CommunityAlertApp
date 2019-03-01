c29d5a82dbe399596abeedfd65dbabaf5e0e01a0
Paul Murphy <murphypaul1985@gmail.com>
2019-03-01 12:07:05 +0000

    Second Commit

--- app/src/main/java/com/paulmurphy/communityalertapp/Activities/CreateAccountActivity.java
+++ app/src/main/java/com/paulmurphy/communityalertapp/Activities/CreateAccountActivity.java
@@ -16,8 +16,11 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.paulmurphy.communityalertapp.R;
+
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -28,12 +31,12 @@ public class CreateAccountActivity extends AppCompatActivity {
     private Button createAccountBtn;
     private DatabaseReference mDatabaseReference;
     private FirebaseDatabase mDatabase;
-    private StorageReference mFirebaseStorage;
+    //private StorageReference mFirebaseStorage;
     private FirebaseAuth mAuth;
     private ProgressDialog mProgressDialog;
-    private ImageButton profilePic;
-    private Uri resultUri = null;
-    private final static int GALLERY_CODE = 1;
+    //private ImageButton profilePic;
+    //private Uri resultUri = null;
+    //private final static int GALLERY_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,13 +48,26 @@ public class CreateAccountActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+        //mFirebaseStorage = FirebaseStorage.getInstance().getReference().child("MBlog_Profile_Pics");
+
         mProgressDialog = new ProgressDialog(this);
 
         firstName = (EditText) findViewById(R.id.firstNameAct);
         lastName = (EditText) findViewById(R.id.lastNameAct);
         email = (EditText) findViewById(R.id.emailAct);
         password = (EditText) findViewById(R.id.passwordAct);
+        //profilePic = (ImageButton) findViewById(R.id.profilePic);
         createAccountBtn = (Button) findViewById(R.id.createAccoutAct);
+
+//        profilePic.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent galleryIntent = new Intent();
+//                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+//                galleryIntent.setType("image/*");
+//                startActivityForResult(galleryIntent, GALLERY_CODE);
+//            }
+//        });
 
         createAccountBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,18 +95,28 @@ public class CreateAccountActivity extends AppCompatActivity {
                 public void onSuccess(AuthResult authResult) {
                     if (authResult != null) {
 
-                        String userid = mAuth.getCurrentUser().getUid();
-                        DatabaseReference currentUserDb = mDatabaseReference.child(userid);
-                        currentUserDb.child("firstname").setValue(name);
-                        currentUserDb.child("lastname").setValue(lname);
-                        currentUserDb.child("image").setValue("none");
+                        //StorageReference imagePath = mFirebaseStorage.child("MBlog_Profile_Pics").child(resultUri.getLastPathSegment());
 
-                        mProgressDialog.dismiss();
+                        //imagePath.putFile(resultUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            //@Override
+                           // public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
 
-                        //send users to postList
-                        Intent intent = new Intent(CreateAccountActivity.this, PostListActivity.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent);
+                                String userid = mAuth.getCurrentUser().getUid();
+                                DatabaseReference currentUserDb = mDatabaseReference.child(userid);
+                                currentUserDb.child("firstname").setValue(name);
+                                currentUserDb.child("lastname").setValue(lname);
+                                currentUserDb.child("image").setValue("none");
+
+                                mProgressDialog.dismiss();
+
+                                //send users to postList
+                                Intent intent = new Intent(CreateAccountActivity.this, PostListActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(intent);
+
+                           // }
+                        //});
+
                     }
 
                 }
@@ -100,5 +126,31 @@ public class CreateAccountActivity extends AppCompatActivity {
 
     }
 
-
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == GALLERY_CODE && resultCode == RESULT_OK) {
+//
+//            Uri mImageUri = data.getData();
+//
+//            CropImage.activity(mImageUri)
+//                    .setAspectRatio(1,1)
+//                    .setGuidelines(CropImageView.Guidelines.ON)
+//                    .start(this);
+//
+//        }
+//
+//        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+//            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+//            if (resultCode == RESULT_OK) {
+//                resultUri = result.getUri();
+//
+//                //profilePic.setImageURI(resultUri);
+//
+//            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+//                Exception error = result.getError();
+//            }
+//        }
+//    }
 }
